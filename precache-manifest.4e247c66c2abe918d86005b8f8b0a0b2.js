e57c342ab1aec96bfa4e36af156c80bc1734bcf8
sabitalizade <sabitalizade@gmail.com>
2021-04-16 12:25:32 +0400

    Updates

---
+++ precache-manifest.4e247c66c2abe918d86005b8f8b0a0b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c97dc658795b3bbc221506df807482",
+    "url": "/sabitalizade.github.io/index.html"
+  },
+  {
+    "revision": "2c27ba6665d096abfb38",
+    "url": "/sabitalizade.github.io/static/css/main.b6634dda.chunk.css"
+  },
+  {
+    "revision": "b01d23f0c8d2b980fb9e",
+    "url": "/sabitalizade.github.io/static/js/2.09cc353e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sabitalizade.github.io/static/js/2.09cc353e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c27ba6665d096abfb38",
+    "url": "/sabitalizade.github.io/static/js/main.a646d6c4.chunk.js"
+  },
+  {
+    "revision": "5cdbead99342593e32c8",
+    "url": "/sabitalizade.github.io/static/js/runtime-main.70f28286.js"
+  },
+  {
+    "revision": "a49d7f5e3ab012728818bcc997c39714",
+    "url": "/sabitalizade.github.io/static/media/coffee-24px.a49d7f5e.svg"
+  },
+  {
+    "revision": "c5732a2b579de52297980f89d1194db3",
+    "url": "/sabitalizade.github.io/static/media/laptop-24px.c5732a2b.svg"
+  },
+  {
+    "revision": "39051bfc39049225c28df28afc5aa6d0",
+    "url": "/sabitalizade.github.io/static/media/list_alt-24px.39051bfc.svg"
+  },
+  {
+    "revision": "488791190702ad609036b8785359924f",
+    "url": "/sabitalizade.github.io/static/media/tungsten-24px.48879119.svg"
+  }
+]);
