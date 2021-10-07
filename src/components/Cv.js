2a7d865e057cf3bb379037627a3c11ec4c5ce36b
sabitalizade <sabitalizade@gmail.com>
2021-10-07 12:31:25 +0400

    statics files already contain data object another files

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -6,211 +6,112 @@ import tungsten from "../icons/tungsten-24px.svg";
 import "../css/Portfolio.css";
 import "../css/Cv.css";
 import ReactToPrint from "react-to-print";
-import { FcPrint } from 'react-icons/fc';
+import { FcPrint } from "react-icons/fc";
+import { data } from "../data";
 
-const Cv = ()=> {
-  const cv = useRef()
+const Cv = () => {
+  const cv = useRef();
+
   return (
     <div className="main">
       <div className="container">
-      <ReactToPrint
+        <ReactToPrint
           trigger={() => {
             // NOTE: could just as easily return <SomeComponent />. Do NOT pass an `onClick` prop
             // to the root node of the returned component as it will be overwritten.
-            return <div className="print"><FcPrint/></div>;
+            return (
+              <div className="print">
+                <FcPrint />
+              </div>
+            );
           }}
           content={() => cv.current}
         />
         <div className="cv" ref={cv}>
           <div className="left">
-              <div className="picture">
-                    <img src="./img/profile.jpg" alt="profile"/>
-              </div>
-              <div className="header-title">
-                Expertise & Skills
-              </div>
-              <div className="skills-list">
-                <ul>
-                  <li>
-                  HTML
-                  </li>
-                  <li>
-                  CSS
-                  </li>
-                  <li>
-                  SASS,SCSS
-                  </li>
-                  <li>
-                  JavaScript
-                  </li>
-                  <li>
-                  React Js, Redux
-                  </li>
-                  <li>
-                  VueJs
-                  </li>
-                  <li>
-                  Node Js,Express Js
-                  </li>
-                  <li>
-                  Mongo DB
-                  </li>
-                   <li>
-                  Docker
-                  </li>
-                  <li>
-                  File Transfer Protocol (FTP)
-                  </li>
-                  <li>
-                  Git & GitHub & GitLab
-                  </li>
-                  <li>
-                  Bootstrap
-                  </li>
-                  </ul>
-                    
-                  </div>
-                   <div className="header-title">Extra knowledge </div> 
-                  <div className="skills-list">
-                  <ul>
-                   <li>
-                  Python,Flask
-                   </li>
-                   <li>
-                  Oracle Sql
-                   </li>
-                   <li>
-                  Php
-                   </li>
-                   <li>
-                  MySql
-                   </li>                  
-                </ul>
-              </div>
-              <div className="header-title">
-              Contact Details
-              </div>
-                  <div className="skills-list">
-                  <ul>
-                <li>
-              Cell: +994 70 671 74 74
-                </li>
-                <li>
-                Cell: +994 60 271 74 71
-                </li>
-                <li>
-              Email:sabitalizade@gmail.com
-                </li>
-                <li>
-                  <address>
-              Address: Baku, Azerbaijan
-                  </address>
-                </li>
-                <li>
-              GitHub: sabitalizade
-                </li>
+            <div className="picture">
+              <img src="./img/sa1.jpg" alt="profile" />
+            </div>
+            <div className="header-title">Expertise & Skills</div>
+            <div className="skills-list">
+              <ul>
+                {data.skills.main.map((item) => (
+                  <li key={item.id}>{item.name}</li>
+                ))}
               </ul>
-                  </div>
-              
+            </div>
+            <div className="header-title">Extra knowledge </div>
+            <div className="skills-list">
+              <ul>
+                {data.skills.extra.map((item) => (
+                  <li key={item.id}>{item.name}</li>
+                ))}
+              </ul>
+            </div>
+            <div className="header-title">Contact Details</div>
+            <div className="skills-list">
+              <ul>
+                {data.contact.map((item) => (
+                  <li key={item.id}>{item.name}</li>
+                ))}
+              </ul>
+            </div>
           </div>
           <div className="right">
-              <div className="name">
-                  Sabit Alizade
-              </div>
-              <div className="prof">
-                Frontend Devoloper
-              </div>
-              <div className="icons">
-                <img src={laptop} alt="" />
-                <img src={coffee} alt="" />
-                <img src={list_alt} alt="" />
-                <img src={tungsten} alt="" />
-              </div>
-              <div className="title">
-               INTRODUCTION
-              </div>
-              <div className="text">
-                <p>
-              I am an experienced Frontend developer who is self-motivated and
-              open to learning. 
-
-                </p>
-                <p>
+            <div className="name">Sabit Alizade</div>
+            <div className="prof">Frontend Devoloper</div>
+            <div className="icons">
+              <img src={laptop} alt="" />
+              <img src={coffee} alt="" />
+              <img src={list_alt} alt="" />
+              <img src={tungsten} alt="" />
+            </div>
+            <div className="title">INTRODUCTION</div>
+            <div className="text">
+              <p>
+                I am an experienced Frontend developer who is self-motivated and
+                open to learning.
+              </p>
+              <p>
                 My strong suits are conceptual thinking and analytical thinking.
-                </p>
-              </div>
-              <div className="title">
-              CAREER SUMMARY
-              </div>
-              <div className="text job" >
-                <p className="job-title">Frontend Devoloper</p>
-                <p className="time">At-Geoetch | June 2021 - present </p>
+              </p>
+            </div>
+            <div className="title">CAREER SUMMARY</div>
+            {data.jobs.map((item) => (
+              <div className="text job" key={item.id}>
+                <p className="job-title">{item.title}</p>
+                <p className="time">{item.time}</p>
                 <ul>
-                  <li>
-                  - I am devoloped internal sistem using Vanila JavaScript, JQuery ReactJs .
-                  </li>                 
-                 
+                  {item.descritions.map((descrition) => (
+                    <li key={descrition.id}>{descrition.name}</li>
+                  ))}
                 </ul>
               </div>
-              <div className="text job" >
-                <p className="job-title">Frontend Devoloper</p>
-                <p className="time">MealperLTE | April 2021 - present </p>
-                <ul>
-                  <li>
-                  - Create Dashboard using React Js.
-                  </li>                 
-                 
-                </ul>
-              </div>
-              <div className="text job">
-                <p className="job-title">Frontend Devoloper</p>
-                <p className="time">Frelancer | May 2020 - present </p>
-                <ul>
-                  <li>
-                  - Creates Statics and dinamics website for client.
-                  </li>
-                  <li>
-                  - Translates static websites into responsive ones.
-                  </li>
-                  
-                </ul>
-              </div>
-           
-              <div className="title m-t">
-              ACADEMIC HISTORY
-              </div>
-              <div className="text">
-                <p className="job-title">BAKU STATE UNIVERSITY</p>
-                <p className="time">Master of Economy </p>
-                <ul>
-                  <li>
-                  - September 2017 - May 2019
-                  </li>
-                  <li>
-                  - GPA 74.7
-                  </li>
-                  
-                </ul>
-              </div>
+            ))}
 
-              <div className="text m-t">
-                <p className="job-title">NAKHCHVAN STATE UNIVERSITY</p>
-                <p className="time">Bachelor of Economy </p>
-                <ul>
-                  <li>
-                  - September 2013 - May 2017
-                  </li>
-                  <li>
-                  - GPA 87.5
-                  </li>
-                  
-                </ul>
-              </div>
+            <div className="title m-t">ACADEMIC HISTORY</div>
+            <div className="text">
+              <p className="job-title">BAKU STATE UNIVERSITY</p>
+              <p className="time">Master of Economy </p>
+              <ul>
+                <li>- September 2017 - May 2019</li>
+                <li>- GPA 74.7</li>
+              </ul>
+            </div>
+
+            <div className="text m-t">
+              <p className="job-title">NAKHCHVAN STATE UNIVERSITY</p>
+              <p className="time">Bachelor of Economy </p>
+              <ul>
+                <li>- September 2013 - May 2017</li>
+                <li>- GPA 87.5</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Cv;
---
+++ src/data.js
@@ -0,0 +1,150 @@
+export const data = {
+    skills : {
+        main:[
+          {
+            id:1,
+            name:"HTML"
+          },
+          {
+            id:2,
+            name:"CSS"
+          },
+          {
+            id:3,
+            name:"SASS,SCSS"
+          },
+          {
+            id:4,
+            name:"JavaScript"
+          },
+          {
+            id:5,
+            name:"React Js, Redux"
+          },
+          {
+            id:6,
+            name:"VueJs"
+          },
+          {
+            id:7,
+            name:" Node Js,Express Js "
+          },
+          {
+            id:8,
+            name:"Mongo DB"
+          },
+          {
+            id:9,
+            name:"Docker"
+          },
+          {
+            id:10,
+            name:"File Transfer Protocol (FTP)"
+          },
+          {
+            id:11,
+            name:"Git & GitHub & GitLab"
+          },
+          {
+            id:12,
+            name:"Bootstrap"
+          },
+          {
+            id:13,
+            name:"Jquery"
+          },
+        ],
+        extra:[
+          {
+            id:1,
+            name:"Python,Flask"
+          },
+          {
+            id:2,
+            name:"Oracle Sql"
+          },
+          {
+            id:3,
+            name:"Php"
+          },
+          {
+            id:4,
+            name:"MySql"
+          },
+          {
+            id:5,
+            name:"React Native"
+          },
+        ]
+      },
+      contact:[
+        {
+            id:1,
+            name:"Cell: +994 70 671 74 74"
+          },
+          {
+            id:2,
+            name:" Cell: +994 60 271 74 71"
+          },
+          {
+            id:3,
+            name:" Email: sabitalizade@gmail.com"
+          },
+        
+          {
+            id:5,
+            name:"Address: Baku, Azerbaijan"
+          },
+          {
+            id:6,
+            name:"GitHub: sabitalizade"
+          },
+      ],
+      jobs:[
+        {
+            id:1,
+            title:"Frontend Devoloper",
+            time:"At-Geoetch | June 2021 - present",
+            descritions:[
+                {
+                    id:1,
+                    name:" - I am devoloped internal sistem using Vanila JavaScript, JQuery ReactJs ."
+                },
+              
+            ]
+     
+          },
+          {
+            id:2,
+            title:"Frontend Devoloper",
+            time:"MealperLTE | April 2021 - present",
+            descritions:[
+                {
+                    id:1,
+                    name:"- Create Dashboard using React Js."
+                },
+              
+            ]
+       
+          },
+          {
+            id:3,
+            title:"Frontend Devoloper",
+            time:"Frelancer | May 2020 - present",
+            descritions:[
+                {
+                    id:1,
+                    name:"- Creates Statics and dinamics website for client."
+                },
+                {
+                    id:2,
+                    name:"- Translates static websites into responsive ones."
+                },
+            ]
+          },
+          
+      ]
+}
+
+
+
