99fe5dbd94f30924331e9c9164f0d2c87423b5b5
sabitalizade <sabitalizade@gmail.com>
2021-10-07 12:59:54 +0400

    portfolio data get from data object

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,262 +1,37 @@
 import React from "react";
 import "../css/Portfolio.css";
+import { data } from "../data";
 
 const Portfolio = ()=> {
   return (
     <div className="main">
       <div className="container hidden">
         <ul className="list">
-          <li>
-            <div className="item">
-              <a
-                href="https://nuhcar.com/"
-                className="image"
-              >
-                <img loading="lazy" src="img/19.png" alt="autonuh"></img>
-              </a>
-              <span className="title">AutoNuh</span>
-              <div className="actions">
-                <a
-                 
-                  href="https://nuhcar.com/"
-                >
-                  View
-                </a>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="item">
-              <a
-                href="https://googlec.vercel.app/"
-                className="image"
-              >
-                <img loading="lazy" src="img/2.png" alt="autonuh"></img>
-              </a>
-              <span className="title">Google Clone</span>
-              <div className="actions">
-                <a
-                 
-                  href="https://googlec.vercel.app/"
-                >
-                  View
-                </a>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="item">
-              <a
-                href="https://covid19trackerapp.vercel.app/"
-                className="image"
-              >
-                <img loading="lazy" src="img/1.png" alt="autonuh"></img>
-              </a>
-              <span className="title">Covid19 tracker with VueJs</span>
-              <div className="actions">
-                <a
-                 
-                  href="https://covid19trackerapp.vercel.app/"
-                >
-                  View
-                </a>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="item">
-              <a
-                href="https://covid19trackerreact.vercel.app/"
-                className="image"
-              >
-                <img loading="lazy" src="img/1.png" alt="autonuh"></img>
-              </a>
-              <span className="title">Covid19 tracker with ReactJs</span>
-              <div className="actions">
-                <a
-                 
-                  href="https://covid19trackerreact.vercel.app/"
-                >
-                  View
-                </a>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="item">
-              <a
-                href="https://shakingtree.vercel.app/"
-                className="image"
-              >
-                <img loading="lazy" src="img/18.png" alt="shaking tree"></img>
-              </a>
-              <span className="title">Shaking Tree</span>
-              <div className="actions">
-                <a
-                 
-                  href="https://shakingtree.vercel.app/"
-                >
-                  View
-                </a>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="item">
-              <a
-                href="https://vuetodoapp.vercel.app/"
-                className="image"
-              >
-                <img loading="lazy" src="img/23.png" alt="shaking tree"></img>
-              </a>
-              <span className="title">VueJs Todo app</span>
-              <div className="actions">
-                <a
-                 
-                  href="https://vuetodoapp.vercel.app/"
-                >
-                  View
-                </a>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="item">
-              <a
-                href="https://sasocket.vercel.app/"
-                className="image"
-              >
-                <img loading="lazy" src="img/25.png" alt="MERN Socket.io chat"></img>
-              </a>
-              <span className="title">MERN Socket.io chat</span>
-              <div className="actions">
-                <a
-                 
-                  href="https://sasocket.vercel.app/"
-                >
-                  View
-                </a>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="item">
-              <a
-                href="https://sachat.vercel.app/"
-                className="image"
-              >
-                <img loading="lazy" src="img/25.png" alt="MERN Pusher chat"></img>
-              </a>
-              <span className="title">MERN Pusher chat</span>
-              <div className="actions">
-                <a
-                 
-                  href="https://sachat.vercel.app/"
-                >
-                  View
-                </a>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="item">
-              <a
-                href="https://brainhouseanticafe.herokuapp.com/"
-                className="image"
-              >
-                <img loading="lazy" src="img/20.jpg" alt="BrainHouse"></img>
-              </a>
-              <span className="title">Brain House</span>
-              <div className="actions">
-                <a
-                 
-                  href="https://brainhouseanticafe.herokuapp.com/"
-                >
-                  View{" "}
-                </a>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="item">
-              <a
-                href="https://alizade.herokuapp.com/"
-                className="image"
-              >
-                <img loading="lazy" src="img/21.jpg" alt="Alizade"></img>
-              </a>
-              <span className="title">Portfolio with React</span>
 
-              <div className="actions">
+           {data.portfolios.map(portfolio=>(
+              <li>
+              <div className="item">
                 <a
-                 
-                  href="https://alizade.herokuapp.com/"
+                  href={portfolio.href}
+                  className="image"
                 >
-                  View{" "}
+                  <img loading="lazy" src={portfolio.imageName} alt="Todo"></img>
                 </a>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="item">
-              <a
-                href="https://sabitalizade.herokuapp.com/"
-                className="image"
-              >
-                <img loading="lazy" src="img/22.jpg" alt="SabitAlizade"></img>
-              </a>
-              <span className="title">Blog with Php</span>
+                <span className="title">{portfolio.title}</span>
+  
+                <div className="actions">
+                  <a
+                   
+                   href={portfolio.href}
 
-              <div className="actions">
-                <a
-                 
-                  href="https://sabitalizade.herokuapp.com/"
-                >
-                  View{" "}
-                </a>
+                  >
+                    View 
+                  </a>
+                </div>
               </div>
-            </div>
-          </li>
-          <li>
-            <div className="item">
-              <a
-                href="https://asyatodo.netlify.app/"
-                className="image"
-              >
-                <img loading="lazy" src="img/23.png" alt="Todo"></img>
-              </a>
-              <span className="title">Todo with React</span>
-
-              <div className="actions">
-                <a
-                 
-                  href="https://asyatodo.netlify.app/"
-                >
-                  View{" "}
-                </a>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div className="item">
-              <a
-                href="https://asya.netlify.app/"
-                className="image"
-              >
-                <img loading="lazy" src="img/24.png" alt="Todo"></img>
-              </a>
-              <span className="title">Chat with React</span>
-
-              <div className="actions">
-                <a
-                 
-                  href="https://asya.netlify.app/"
-                >
-                  View{" "}
-                </a>
-              </div>
-            </div>
-          </li>
+            </li>
+           ))}
+         
         </ul>
       </div>
     </div>
--- src/data.js
+++ src/data.js
@@ -1,150 +1,222 @@
 export const data = {
-    skills : {
-        main:[
-          {
-            id:1,
-            name:"HTML"
-          },
-          {
-            id:2,
-            name:"CSS"
-          },
-          {
-            id:3,
-            name:"SASS,SCSS"
-          },
-          {
-            id:4,
-            name:"JavaScript"
-          },
-          {
-            id:5,
-            name:"React Js, Redux"
-          },
-          {
-            id:6,
-            name:"VueJs"
-          },
-          {
-            id:7,
-            name:" Node Js,Express Js "
-          },
-          {
-            id:8,
-            name:"Mongo DB"
-          },
-          {
-            id:9,
-            name:"Docker"
-          },
-          {
-            id:10,
-            name:"File Transfer Protocol (FTP)"
-          },
-          {
-            id:11,
-            name:"Git & GitHub & GitLab"
-          },
-          {
-            id:12,
-            name:"Bootstrap"
-          },
-          {
-            id:13,
-            name:"Jquery"
-          },
-        ],
-        extra:[
-          {
-            id:1,
-            name:"Python,Flask"
-          },
-          {
-            id:2,
-            name:"Oracle Sql"
-          },
-          {
-            id:3,
-            name:"Php"
-          },
-          {
-            id:4,
-            name:"MySql"
-          },
-          {
-            id:5,
-            name:"React Native"
-          },
-        ]
+  skills: {
+    main: [
+      {
+        id: 1,
+        name: "HTML",
       },
-      contact:[
+      {
+        id: 2,
+        name: "CSS",
+      },
+      {
+        id: 3,
+        name: "SASS,SCSS",
+      },
+      {
+        id: 4,
+        name: "JavaScript",
+      },
+      {
+        id: 5,
+        name: "React Js, Redux",
+      },
+      {
+        id: 6,
+        name: "VueJs",
+      },
+      {
+        id: 7,
+        name: " Node Js,Express Js ",
+      },
+      {
+        id: 8,
+        name: "Mongo DB",
+      },
+      {
+        id: 9,
+        name: "Docker",
+      },
+      {
+        id: 10,
+        name: "File Transfer Protocol (FTP)",
+      },
+      {
+        id: 11,
+        name: "Git & GitHub & GitLab",
+      },
+      {
+        id: 12,
+        name: "Bootstrap",
+      },
+      {
+        id: 13,
+        name: "Jquery",
+      },
+    ],
+    extra: [
+      {
+        id: 1,
+        name: "Python,Flask",
+      },
+      {
+        id: 2,
+        name: "Oracle Sql",
+      },
+      {
+        id: 3,
+        name: "Php",
+      },
+      {
+        id: 4,
+        name: "MySql",
+      },
+      {
+        id: 5,
+        name: "React Native",
+      },
+    ],
+  },
+  contact: [
+    {
+      id: 1,
+      name: "Cell: +994 70 671 74 74",
+    },
+    {
+      id: 2,
+      name: " Cell: +994 60 271 74 71",
+    },
+    {
+      id: 3,
+      name: " Email: sabitalizade@gmail.com",
+    },
+
+    {
+      id: 5,
+      name: "Address: Baku, Azerbaijan",
+    },
+    {
+      id: 6,
+      name: "GitHub: sabitalizade",
+    },
+  ],
+  jobs: [
+    {
+      id: 1,
+      title: "Frontend Devoloper",
+      time: "At-Geoetch | June 2021 - present",
+      descritions: [
         {
-            id:1,
-            name:"Cell: +994 70 671 74 74"
-          },
-          {
-            id:2,
-            name:" Cell: +994 60 271 74 71"
-          },
-          {
-            id:3,
-            name:" Email: sabitalizade@gmail.com"
-          },
-        
-          {
-            id:5,
-            name:"Address: Baku, Azerbaijan"
-          },
-          {
-            id:6,
-            name:"GitHub: sabitalizade"
-          },
+          id: 1,
+          name: " - I am devoloped internal sistem using Vanila JavaScript, JQuery ReactJs .",
+        },
       ],
-      jobs:[
+    },
+    {
+      id: 2,
+      title: "Frontend Devoloper",
+      time: "MealperLTE | April 2021 - present",
+      descritions: [
         {
-            id:1,
-            title:"Frontend Devoloper",
-            time:"At-Geoetch | June 2021 - present",
-            descritions:[
-                {
-                    id:1,
-                    name:" - I am devoloped internal sistem using Vanila JavaScript, JQuery ReactJs ."
-                },
-              
-            ]
-     
-          },
-          {
-            id:2,
-            title:"Frontend Devoloper",
-            time:"MealperLTE | April 2021 - present",
-            descritions:[
-                {
-                    id:1,
-                    name:"- Create Dashboard using React Js."
-                },
-              
-            ]
-       
-          },
-          {
-            id:3,
-            title:"Frontend Devoloper",
-            time:"Frelancer | May 2020 - present",
-            descritions:[
-                {
-                    id:1,
-                    name:"- Creates Statics and dinamics website for client."
-                },
-                {
-                    id:2,
-                    name:"- Translates static websites into responsive ones."
-                },
-            ]
-          },
-          
-      ]
-}
-
-
-
+          id: 1,
+          name: "- Create Dashboard using React Js.",
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "Frontend Devoloper",
+      time: "Frelancer | May 2020 - present",
+      descritions: [
+        {
+          id: 1,
+          name: "- Creates Statics and dinamics website for client.",
+        },
+        {
+          id: 2,
+          name: "- Translates static websites into responsive ones.",
+        },
+      ],
+    },
+  ],
+  portfolios: [
+    {
+      id: 1,
+      href: "https://nuhcar.com/",
+      imageName: "img/19.png",
+      title: "AutoNuh",
+    },
+    {
+      id: 2,
+      href: "https://googlec.vercel.app/",
+      imageName: "img/2.png",
+      title: "Google Clone",
+    },
+    {
+      id: 3,
+      href: "https://covid19trackerapp.vercel.app/",
+      imageName: "img/1.png",
+      title: "Covid19 tracker with VueJs",
+    },
+    {
+      id: 4,
+      href: "https://covid19trackerreact.vercel.app/",
+      imageName: "img/18.png",
+      title: "Shaking Tree",
+    },
+    {
+      id: 5,
+      href: "https://shakingtree.vercel.app/",
+      imageName: "img/18.png",
+      title: "Shaking Tree",
+    },
+    {
+      id: 6,
+      href: "https://vuetodoapp.vercel.app/",
+      imageName: "img/23.png",
+      title: "VueJs Todo app",
+    },
+    {
+      id: 7,
+      href: "https://sasocket.vercel.app/",
+      imageName: "img/25.png",
+      title: "MERN Socket.io chat",
+    },
+    {
+      id: 8,
+      href: "https://sachat.vercel.app/",
+      imageName: "img/25.png",
+      title: "MERN Pusher chat",
+    },
+    {
+      id: 9,
+      href: "https://brainhouseanticafe.herokuapp.com/",
+      imageName: "img/20.jpg",
+      title: "Brain House",
+    },
+    {
+      id: 10,
+      href: "https://alizade.herokuapp.com/",
+      imageName: "img/21.jpg",
+      title: "Portfolio with React",
+    },
+    {
+      id: 11,
+      href: "https://sabitalizade.herokuapp.com/",
+      imageName: "img/22.jpg",
+      title: "Blog with Php",
+    },
+    {
+      id: 12,
+      href: "https://asyatodo.netlify.app/",
+      imageName: "img/23.png",
+      title: "Todo with React",
+    },
+    {
+      id: 13,
+      href: "https://asya.netlify.app/",
+      imageName: "img/24.png",
+      title: "Chat with React",
+    },
+  ],
+};
