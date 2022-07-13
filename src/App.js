e7d384938dc01b4318f54125bc6f838fb496733f
sabitalizade <sabitalizade@gmail.com>
2022-07-13 16:42:48 +0400

    home page design chaged to lootie file and packages updated

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import React, {   useEffect, useState } from 'react';
-import { Route,useLocation } from "react-router-dom";
-import './App.scss';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Context from './components/Context';
-import SideBar from './components/SideBar';
-import Logo from './components/Logo';
-import Menu from './components/Menu';
-import Cv from './components/Cv';
+import React, { useEffect, useState } from "react";
+import { Route, useLocation,Routes } from "react-router-dom";
+import "./App.scss";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Context from "./components/Context";
+import SideBar from "./components/SideBar";
+import Logo from "./components/Logo";
+import Menu from "./components/Menu";
+import Cv from "./components/Cv";
 
-const App = ()=> {
-     const [href, sethref] = useState(false)
-     const {pathname} = useLocation(); 
- 
-useEffect(() => {
-     pathname==="/cv"?sethref(true):sethref(false)
-}, [pathname])
+const App = () => {
+  const [href, sethref] = useState(false);
+  const { pathname } = useLocation();
 
+  useEffect(() => {
+    pathname === "/cv" ? sethref(true) : sethref(false);
+  }, [pathname]);
 
   return (
-       <>
-    <div className="App">
-    <div className={href?"Header HeaderCv":"Header "}>
-        <Logo href={href}/>
-         <Menu/>
-    </div>
-   <div className="MainBar">
-        <SideBar href={href}/>     
-         <Route path="/" exact component={Context}/>
-         <Route path="/portfolio" component={Portfolio}/>
-         <Route path="/contact" component={Contact}/>
-         <Route path="/cv" component={Cv}/>
-
-    </div>
-    </div>
+    <>
+      <div className="App">
+        <div className={href ? "Header HeaderCv" : "Header "}>
+          <Logo href={href} />
+          <Menu />
+        </div>
+        <div className="MainBar">
+          <SideBar href={href} />
+          <Routes>
+            <Route path="/" element={<Context/>} />
+            <Route path="/portfolio" element={<Portfolio/>} />
+            <Route path="/contact" element={<Contact/>} />
+            <Route path="/cv" element={<Cv/>} />
+          </Routes>
+        </div>
+      </div>
     </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {  BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+// import { Provider } from "react-redux";
+// import { store } from "app/store";
+// import "antd/dist/antd.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router> 
-    <App />
-    </Router>
-
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render( 
+    <BrowserRouter>
+      <App />
+    </BrowserRouter> 
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// reportWebVitals();
