ce2db2543cee2f65ac4449c8e6a1f5669a495a0d
sabitalizade <sabitalizade@gmail.com>
2023-01-28 21:35:45 +0400

    bug fix for pdf viewer

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,12 +1,12 @@
-import React from "react"; // , { useRef }
-import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
+import React from "react";
+import { Document, Page, pdfjs } from "react-pdf/dist/esm/entry.webpack5";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import { FcPrint } from "react-icons/fc";
-
+pdfjs.GlobalWorkerOptions.workerSrc = `./pdf.worker.js`;
 const Cv = () => {
   return (
-    <div className="w-min mx-auto ">
+    <div className="w-min mx-auto">
       <a
         href="./files/sabitalizade.pdf"
         target="_blank"
@@ -21,17 +21,7 @@ const Cv = () => {
           pageNumber={1}
           scale={1.3}
           loading={
-            <div
-              style={{
-                width: "100%",
-                height: "100%",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                fontSize: "2rem",
-                color: "white",
-              }}
-            >
+            <div className="w-full h-full flex justify-center items-center text-3xl text-white">
               Loading...
             </div>
           }
